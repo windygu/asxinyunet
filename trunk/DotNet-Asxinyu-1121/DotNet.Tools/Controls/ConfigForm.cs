86941676ac841ddc059d1bef1707ba8c34564bbe
asxinyu@qq.com <asxinyu@qq.com@e81cc8ed-56e0-f867-87c4-5ecea47cd9ab>
2011-11-21 13:21:27 +0000

    1121

---
+++ trunk/DotNet-Asxinyu-1121/DotNet.Tools/Controls/ConfigForm.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： asxinyu}
+ * 日期: 2011-9-25
+ * 时间: 20:02
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using XCode.Configuration ;
+using XCode ;
+using NewLife.Configuration ;
+
+namespace DotNet.Tools.Controls
+{
+	/// <summary>
+	/// Description of ConfigForm.
+	/// </summary>
+	public partial class ConfigForm : UserControl
+	{
+		public ConfigForm()
+		{
+			InitializeComponent();			
+		}
+		
+		void ToolAdd_Click(object sender, EventArgs e)
+		{
+			// TODO: Implement ToolAdd_Click
+		}
+		
+		void ToolFind_Click(object sender, EventArgs e)
+		{
+			//加载所有的配置文件信息
+			//建立配置文件数据表来操作更方便
+		}
+	}
+}
---
+++ trunk/DotNet-Asxinyu-1121/LotteryTicket/LotteryTicket/Test/TestData.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 董斌辉
+ * Date: 2011-4-5
+ * Time: 7:59
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO ;
+
+namespace LotteryTicket
+{
+	/// <summary>
+	/// 处理测试数据
+	/// </summary>
+	public class TestData
+	{
+		public static double[][] ReadTestDataFromTxt()
+		{
+			int NumberCount = 7 ;//定义的原始数据个数
+			string path = @"../../TestData/AllDataCalculateData.txt" ;
+			StreamReader sr = new StreamReader(path );
+            string line = null;
+            double[][] testData = new double[NumberCount][] ;
+            int count = 0 ;
+            while ( (line = sr.ReadLine())!=null )
+            {
+            	if (line.StartsWith("#")||(line.Length <4))
+                {
+                    continue;
+                }
+            	if (line.StartsWith ("&"))
+            	{
+            		string[] tempStr = line.Trim ().Substring (1).Split (',') ;
+            		System.Collections.Generic.List<double > tempList = 
+            			new System.Collections.Generic.List<double> ();
+            		for (int i = 0 ; i <tempStr.Length ; i ++)
+            		{
+            			tempList.Add (Double.Parse (tempStr [i ])) ;
+            		}
+            		testData [count ++] = tempList.ToArray () ;
+            		continue ;
+            	}
+            }
+            sr.Close () ;
+            return testData ;
+		}
+	}
+}
