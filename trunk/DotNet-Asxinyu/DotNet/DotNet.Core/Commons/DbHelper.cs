6d8d1305ff34870e80567e6d33bdb61e9db87da8
asxinyu@qq.com <asxinyu@qq.com@e81cc8ed-56e0-f867-87c4-5ecea47cd9ab>
2012-04-25 12:21:58 +0000

    增加拷贝数据库功能

--- trunk/DotNet-Asxinyu/DotNet/DotNet.Core/Commons/DbHelper.cs
+++ trunk/DotNet-Asxinyu/DotNet/DotNet.Core/Commons/DbHelper.cs
@@ -52,7 +52,7 @@ namespace DotNet.Core.Commons
                         }
                     }
                     model.Save();                   
-                    //Console.WriteLine("表:{0}的第{1} 条记录插入完成！",tableList[t].Name ,i);
+                    //
                 }
             }
         }
@@ -73,7 +73,7 @@ namespace DotNet.Core.Commons
             {
                 //首先根据表名称获取当前表的实体操作接口
                 IEntityOperate entity = dal.CreateOperate(item.Name);
-                for (int i = 0; i < NeedCount; i++)
+                for (int i = 0; i < needCount; i++)
                 {
                     IEntity model = entity.Create();//创建数据实体接口
                     FieldItem[] filds = entity.Fields;//获取所有的字段信息
@@ -81,9 +81,10 @@ namespace DotNet.Core.Commons
                     {
                         if (!fild.IsIdentity)
                             model.SetItem(fild.Name, GetRandomValue(fild));
-                    }
+                    }                    
                     model.Save();//保存数据
                 }
+                Console.WriteLine("数据表{0} 数据插入完成！",item.Name );
             }
         }
 
@@ -114,6 +115,56 @@ namespace DotNet.Core.Commons
                 default:
                     return string.Empty;
             }
+        }
+        #endregion
+
+        #region 拷贝数据库
+        /// <summary>
+        /// 拷贝数据库，只需要数据库连接字符串和源数据库即可
+        /// </summary>
+        /// <param name="originConn">源数据库连接字符串</param>
+        /// <param name="desConn">目的数据库连接字符串</param>
+        /// <param name="perCount">每次获取的记录数目，如果默认-1则会自动调用函数计算一个合理值</param>
+        public static void CopyDataBase(string originConn,string desConn,int perCount = -1)
+        {
+            //思路：通过源数据库获取架构信息，然后反向工程,然后导出数据            
+            DAL dal = DAL.Create(originConn);
+            List<IDataTable> tableList = dal.Tables;//获取源数据库的架构信息
+            tableList.RemoveAll(t => t.IsView);//过滤掉视图
+            //首先拷贝数据库架构            
+            DAL desDal = DAL.Create(desConn);//要在配置文件中启用数据库架构才行 
+            desDal.Db.CreateMetaData().SetTables(tableList.ToArray());               
+            //然后依次拷贝每个表中的数据
+            foreach (var item in tableList)
+            {
+                //首先根据表名称获取当前表的实体操作接口
+                IEntityOperate Factory = dal.CreateOperate(item.Name);
+                //分页获取数据，并更新到新的数据库，通过更改数据库连接来完成
+                int allCount = Factory.FindCount ();
+                if (perCount < 0) perCount = GetDataRowsPerConvert (allCount );
+                int pages = (int)Math.Ceiling ((double)((double )allCount/(double )perCount));
+                for (int i = 0; i < pages ; i++)
+                {
+                    Factory.ConnName = originConn;
+                    IEntityList modelList = Factory.FindAll(string.Empty, string.Empty, string.Empty, i * perCount, perCount);
+                    Factory.ConnName = desConn;
+                    modelList.Insert(true);
+                }
+                Console.WriteLine("数据库{0} 数据转移完成！",item.Name );
+            }
+        }
+
+        /// <summary>
+        /// 根据数据表的记录总数来设置一个合理的每次转换数目。数据量大，一次性导出导入不合理
+        /// </summary>
+        /// <param name="allCount">数据表记录总数</param>
+        /// <returns>每次转换的记录数</returns>
+        private static int GetDataRowsPerConvert(int allCount)
+        {
+            if (allCount < 1000) return 200;
+            else if (allCount < 5000) return 500;
+            else if (allCount < 50000) return 1000;
+            else return 1500;
         }
         #endregion
     }
--- trunk/DotNet-Asxinyu/Test/Program.cs
+++ trunk/DotNet-Asxinyu/Test/Program.cs
@@ -13,22 +13,34 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            //不用实体来操作数据库Demo
-            DAL dal = DAL.Create("Common");
-            IEntityOperate entity = dal.CreateOperate("Administrator");
-            for (int i = 0; i < 1000; i++)
-            {
-                IEntity model = entity.Create();
-                FieldItem[] filds = entity.Fields;
-                int fildsCount = filds.Count();
-                for (int j = 0; j < fildsCount; j++)
-                {
-                    if (!filds[j].IsIdentity)
-                    {
-                        model.SetItem(filds[j].Name, 32);
-                    }
-                }
-            }           
+            CopyDataBase("Common", "Common3");
         }
+        #region 拷贝数据库
+        /// <summary>
+        /// 拷贝数据库，只需要数据库连接字符串和源数据库即可
+        /// </summary>
+        /// <param name="originConn">源数据库连接字符串</param>
+        /// <param name="desConn">目的数据库连接字符串</param>
+        public static void CopyDataBase(string originConn, string desConn)
+        {
+            //思路：通过源数据库获取架构信息，然后反向工程
+            //然后批量导出数据
+            DAL dal = DAL.Create(originConn);
+            List<IDataTable> tableList = dal.Tables;//获取源数据库的架构信息
+            tableList.RemoveAll(t => t.IsView);//过滤掉视图
+            //首先拷贝数据库架构            
+            DAL desDal = DAL.Create(desConn);//要在配置文件中启用数据库架构才行 
+            //desDal.Db.CreateMetaData().SetTables(tableList.ToArray ());               
+            //然后依次拷贝每个表中的数据
+            foreach (var item in tableList)
+            {
+                //首先根据表名称获取当前表的实体操作接口
+                IEntityOperate Factory = dal.CreateOperate(item.Name);               
+                IEntityList modelList = Factory.FindAll();
+                Factory.ConnName = desConn;
+                modelList.Insert(true);
+            }
+        }
+        #endregion
     }
 }
