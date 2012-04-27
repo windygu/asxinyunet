8cda7fcda8007b046f66c1973df228bc1440a532
asxinyu <asxinyu@qq.com>
2012-04-27 01:38:31 +0000

    增加编号生成类，第一个初始版本，通过简单的测试

--- DotNet-Asxinyu/StudySample/DesignPattern/Program.cs
+++ DotNet-Asxinyu/StudySample/DesignPattern/Program.cs
@@ -8,16 +8,41 @@ using DotNet.Core.Commons ;
 
 namespace DesignPattern
 {
-    public class Test
+    public class SequenceTest
     {
+        //配置第一个单元：部门编号 JSB,3个字符，分隔符为-，不是流水号,填充可以不设置或者随意设置
+        static SeqUnitSetting s1 = new SeqUnitSetting("JSB", 3, false, false, true, '0', '-');
+        //配置第二个单元：按照当前系统时间的日期来，6位，分隔符，如果不需要流水号，那么就不要设置分隔符
+        static SeqUnitSetting s2 = new SeqUnitSetting("yyMMdd", 6, true, false, false);
+        //配置最后一个单元：流水号，一般单据号最后是变动的数字,填充#号，3位
+        static SeqUnitSetting s3 = new SeqUnitSetting("", 3, false, true, true, '#');     
+        public static string GetSequence()
+        {
+            return s1.CurrentSquenceString + s2.CurrentSquenceString + s3.GetCurrentSerialNumber ();
+        }
+    }
+
+    public class Test
+    {       
         static void Main(string[] args)
         {
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(SequenceTest.GetSequence());
+            //}
             //DbHelper.FillDataForDb("Common", 50);
             //DbHelper.CopyDataBase("Common", "CommonMysql", 20);
             DbHelper.CopyDataBase("CommonMysql", "CommonSqlite", 50);
             //DbHelper.CopyDataBase("CommonSqlite", "Common", 50);
+            Console.ReadKey();
+            
         }
-        
+        static void TestSequence()
+        {
+            SeqUnitSetting s1 = new SeqUnitSetting("JSB", 3, false, false, true, '0','-');
+            SeqUnitSetting s2 = new SeqUnitSetting("yyMMdd", 6, true, false, false);
+            Console.WriteLine (s1.CurrentSquenceString  +s2.CurrentSquenceString  +"001");
+        }
         /// 数据集合、哈希表及链表的速度测试,验证石头的总结：
         /// 数据行集合，索引访问第一
         /// 哈希桶集合，查找排序第一
