dcc7cd917ee61d568652b40b4ccc654df10613d0
asxinyu@qq.com <asxinyu@qq.com@e81cc8ed-56e0-f867-87c4-5ecea47cd9ab>
2012-05-04 16:28:17 +0000

    快速模板引擎调用

--- trunk/DotNet-Asxinyu/DotNet/DotNet.Tools/DotWMITemplate.cs
+++ trunk/DotNet-Asxinyu/DotNet/DotNet.Tools/DotWMITemplate.cs
@@ -6,7 +6,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using XTemplate;
+using XTemplate.Templating ;
 
 namespace DotNet.Tools
 {
@@ -20,7 +20,7 @@ namespace DotNet.Tools
         private void btnGeneration_Click(object sender, EventArgs e)
         {
             Dictionary<String, Object> data = new Dictionary<String, Object>(); 
-            data["name"] = "参数测试";
+            data["name"] = "参数测试";            
             string content =Template.ProcessTemplate("模版文件.txt", data);
         }
     }
