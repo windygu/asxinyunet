ec18a337e71dd900457b4d192925685a98ab2fc5
asxinyu@qq.com <asxinyu@qq.com@e81cc8ed-56e0-f867-87c4-5ecea47cd9ab>
2012-05-04 14:25:48 +0000

    增加多期基类，并修改多期重复的循环Bug

---
+++ trunk/DotNet-Asxinyu/LotteryTicket/LotTick/指标计算/多期指标/Index_红多期基类.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+namespace LotTick
+{
+    /// <summary>
+    /// 红多期基类
+    /// </summary>
+    public class Index_红多期基类 : LotIndex
+    {
+        public override int[] GetAllValue(LotTickData[] data)
+        {
+            return base.GetAllValue(data);
+        }
+        public override LotTickData[] GetFilterResult(LotTickData[] data, LotTickData[] NeedData = null)
+        {
+            return base.GetFilterResult(data, NeedData);
+        }
+        public override bool[] GetValidateResult(LotTickData[] data)
+        {
+            return GetAllValue(data).Select(n => n.GetCompareResult(this.RuleInfoParams)).ToArray();
+        }
+    }
+}
--- trunk/DotNet-Asxinyu/LotteryTicket/LotTick/指标计算/多期指标/Index_红邻号出现数.cs
+++ trunk/DotNet-Asxinyu/LotteryTicket/LotTick/指标计算/多期指标/Index_红邻号出现数.cs
@@ -5,11 +5,11 @@ namespace LotTick
     /// <summary>
     /// 上期的邻号出现个数（红球）
     /// </summary>
-    public class Index_红邻号出现数 : LotIndex
+    public class Index_红邻号出现数 : Index_红多期基类 
     {
         //根据需要计算的行数来循环计算,输入数据已经考虑了所需行
         public override int[] GetAllValue(LotTickData[] data)
-        {  
+        {
             int[] res = new int[data.Length  - this.RuleInfoParams.NeedRows]; 
             for (int i = 0; i < res.Length ; i++)
             {
@@ -18,14 +18,9 @@ namespace LotTick
             return res;
         }
         public override LotTickData[] GetFilterResult(LotTickData[] data, LotTickData[] NeedData = null)
-        {            
-            //LotTickData lastData = NeedData[0 ];去需要的第一个个数据来进行对比计算
+        {  //LotTickData lastData = NeedData[0 ];去需要的第一个个数据来进行对比计算
             return data.Where(n => (n.NormalData.Index_S邻号出现个数(NeedData[0].NormalData)).
                 GetCompareResult(this.RuleInfoParams)).ToArray();
-        }
-        public override bool[] GetValidateResult(LotTickData[] data)
-        {
-            return GetAllValue(data).Select(n => n.GetCompareResult(this.RuleInfoParams)).ToArray();
         }
     }
 }
