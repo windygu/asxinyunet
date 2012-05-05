bc2c65881f0b7ecea333f5d45c6fcd42e01e99e3
asxinyu <asxinyu@qq.com>
2012-05-05 06:50:18 +0000

    增加StringBulidor转换器

--- DotNet-Asxinyu/DotNet/DotNet.Tools/DotWMITemplate.cs
+++ DotNet-Asxinyu/DotNet/DotNet.Tools/DotWMITemplate.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Windows.Forms;
 using XTemplate.Templating ;
 using DotNet.WinForm.Controls;
+using System.IO;
 
 namespace DotNet.Tools
 {
@@ -20,23 +21,56 @@ namespace DotNet.Tools
         private void btnGeneration_Click(object sender, EventArgs e)
         {
             Dictionary<String, Object> data = GetData();
+            data.Add("NameSpace", txtNameSpace.Text);
+            data.Add("ClassName", txtClassName.Text);           
             //将元数据添加到data中去,字段名称作为key，字段类型为 value
-            string content =Template.ProcessTemplate("WMI模板.txt", data);
+            //需要读入模板内容
+            string tempContent = File.ReadAllText("WMI模板.cs");
+            string content = Template.ProcessTemplate(tempContent, data);
+            String dir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, txtOutputFolder.Text.Trim (),
+                txtClassName.Text .Trim ()+".cs");
+            StreamWriter fs = File.CreateText(dir );
+            fs.Write(content);
+            fs.Close();
         }
+        /// <summary>
+        /// 获取元数据，注意要进行类型转换,value
+        /// </summary>
+        /// <returns></returns>
         private Dictionary<string, object> GetData()
         {
             Dictionary<String, Object> data = new Dictionary<String, Object>();
-            string[] text = txtOriginText.Text.Trim().Split(';'); //分割符为;号
+            string[] text = txtOriginText.Text.Trim().Replace("\r\n","").Split(';'); //分割符为;号
             foreach (var item in text )
             {
-                string[] element = item.Split(' ');
+                string[] element = item.Trim ().Split(new[]{' '}, StringSplitOptions.RemoveEmptyEntries);
                 if (element .Length ==2)
                 {
-                    data.Add(element[1].Trim(), element[0].Trim());
+                    data.Add(element[1].Trim(), GetNewType (element[0].Trim()));
                 }
-            }
+            }        
+             
             return data;
         }
+        /// <summary>
+        /// 类型转换
+        /// </summary>
+        string GetNewType(string oldType)
+        {
+            switch (oldType )
+            {
+                case "uint16": return "UInt16";
+                case "string": return "string";
+                case "uint32": return "UInt32";
+                case "boolean": return "bool";
+                case "datetime": return "DateTime";
+                case "uint64": return "UInt64";             
+                default: return oldType;                    
+            }
+        }
+        /// <summary>
+        /// 创建窗体
+        /// </summary>        
         public static FormModel CreateForm()
         {
            return DotNet.WinForm.WinFormHelper.GetControlForm(new DotWMITemplate (), "WMI封装模板");
--- DotNet-Asxinyu/DotNet/DotNet.Tools/MainForm.Designer.cs
+++ DotNet-Asxinyu/DotNet/DotNet.Tools/MainForm.Designer.cs
@@ -28,21 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.wMI代码生成ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stringBuilder转换ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.wMI代码生成ToolStripMenuItem,
+            this.stringBuilder转换ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(684, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // wMI代码生成ToolStripMenuItem
+            // 
+            this.wMI代码生成ToolStripMenuItem.Name = "wMI代码生成ToolStripMenuItem";
+            this.wMI代码生成ToolStripMenuItem.Size = new System.Drawing.Size(83, 20);
+            this.wMI代码生成ToolStripMenuItem.Text = "WMI代码生成";
+            this.wMI代码生成ToolStripMenuItem.Click += new System.EventHandler(this.wMI代码生成ToolStripMenuItem_Click);
+            // 
+            // stringBuilder转换ToolStripMenuItem
+            // 
+            this.stringBuilder转换ToolStripMenuItem.Name = "stringBuilder转换ToolStripMenuItem";
+            this.stringBuilder转换ToolStripMenuItem.Size = new System.Drawing.Size(119, 20);
+            this.stringBuilder转换ToolStripMenuItem.Text = "StringBuilder转换";
+            this.stringBuilder转换ToolStripMenuItem.Click += new System.EventHandler(this.stringBuilder转换ToolStripMenuItem_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(684, 469);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DotNet工具集合";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem wMI代码生成ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stringBuilder转换ToolStripMenuItem;
     }
 }
 
