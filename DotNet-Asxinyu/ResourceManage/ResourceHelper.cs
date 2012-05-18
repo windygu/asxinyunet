8a2b682256c430540aa05ca44c363f8bc651a8ed
asxinyu <asxinyu@qq.com>
2012-05-18 06:58:22 +0000

    资源管理初始功能，扫描文件夹，添加文件信息

--- DotNet-Asxinyu/ResourceManage/ResourceHelper.cs
+++ DotNet-Asxinyu/ResourceManage/ResourceHelper.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
-using System.Text;
+using System.Collections;
 
 namespace ResourceManage
 {
@@ -15,7 +15,44 @@ namespace ResourceManage
         public static void ScanFolder(string folderName)
         {
             //扫描指定目录下的所有文件,并添加到数据库
+            ArrayList resArray = new ArrayList();
+            string[] files = Directory.GetFiles(folderName, "*");
+            //将当前文件信息添加到数据库
+            foreach (var item in files )
+            {
+                if (File.Exists(item))
+                    GetFileInfo(item);//添加到数据库
+            }
+            Console.WriteLine("文件夹扫描完成:{0}",folderName );
+            string[] folders = Directory.GetDirectories(folderName);
+            //遍历所有文件夹
+            foreach (var item in folders) ScanFolder(item);            
+        }
 
+        public static void GetFileInfo(string fileName)
+        {
+            ResourceInfo model = new ResourceInfo();            
+            model.Md5 = MD5Hash(fileName);
+            if (ResourceInfo.FindAllByName(ResourceInfo._.Md5, model.Md5, "", 0, 0).Count > 0)
+            {
+                model.PublishingCompany = "暂无";
+                FileInfo fi = new FileInfo(fileName);
+                model.Keywords = fi.Name;
+                model.Author = "暂无";
+                model.BigCategory = "暂无分类";
+                model.SmallCategory = "暂无分类";
+                model.Source = "网络";
+                model.ContentIntroduce = "暂无介绍";
+                model.Format = fi.Extension.Replace(".", "");
+                model.ISBN = string.Empty;
+                model.PublishingDate = DateTime.Now;
+                model.Remark = string.Empty;
+                model.ResourceName = fi.Name;
+                model.ResourceScore = 7;
+                model.Size = (int)(fi.Length / 1024);//Kb
+                model.StorageLocation = fileName.Substring(1, fileName.Length - 1);//只存储相对位置，把盘符去掉               
+                model.Insert();
+            }
         }
 
         /// <summary>
