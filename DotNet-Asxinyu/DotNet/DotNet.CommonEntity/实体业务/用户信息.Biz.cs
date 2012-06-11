b1368b61874dd0b9d7a7babfdd013b04fd51a604
asxinyu <asxinyu@qq.com>
2012-06-11 08:32:22 +0000

    2012-06-11： 1.初步修改实体类，还需进一步整理TODO列表

--- DotNet-Asxinyu/DotNet/DotNet.CommonEntity/实体业务/用户信息.Biz.cs
+++ DotNet-Asxinyu/DotNet/DotNet.CommonEntity/实体业务/用户信息.Biz.cs
@@ -22,38 +22,6 @@ namespace DotNet.CommonEntity
             // 用于引发基类的静态构造函数，所有层次的泛型实体类都应该有一个
             TEntity entity = new TEntity();
         }
-
-        ///// <summary>已重载。基类先调用Valid(true)验证数据，然后在事务保护内调用OnInsert</summary>
-        ///// <returns></returns>
-        //public override Int32 Insert()
-        //{
-        //    return base.Insert();
-        //}
-
-        ///// <summary>已重载。在事务保护范围内处理业务，位于Valid之后</summary>
-        ///// <returns></returns>
-        //protected override Int32 OnInsert()
-        //{
-        //    return base.OnInsert();
-        //}
-
-        ///// <summary>验证数据，通过抛出异常的方式提示验证失败。</summary>
-        ///// <param name="isNew"></param>
-        //public override void Valid(Boolean isNew)
-        //{
-        //    // 建议先调用基类方法，基类方法会对唯一索引的数据进行验证
-        //    base.Valid(isNew);
-
-        //    // 这里验证参数范围，建议抛出参数异常，指定参数名，前端用户界面可以捕获参数异常并聚焦到对应的参数输入框
-        //    if (String.IsNullOrEmpty(Name)) throw new ArgumentNullException(_.Name, _.Name.Description + "无效！");
-        //    if (!isNew && ID < 1) throw new ArgumentOutOfRangeException(_.ID, _.ID.Description + "必须大于0！");
-
-        //    // 在新插入数据或者修改了指定字段时进行唯一性验证，CheckExist内部抛出参数异常
-        //    if (isNew || Dirtys[_.Name]) CheckExist(_.Name);
-        //    if (isNew || Dirtys[_.Name] || Dirtys[_.DbType]) CheckExist(_.Name, _.DbType);
-        //    if ((isNew || Dirtys[_.Name]) && Exist(_.Name)) throw new ArgumentException(_.Name, "值为" + Name + "的" + _.Name.Description + "已存在！");
-        //}
-
         /// <summary>已重载。删除关联数据</summary>
         /// <returns></returns>
         protected override int OnDelete()
@@ -65,7 +33,7 @@ namespace DotNet.CommonEntity
 
             return base.OnDelete();
         }
-
+        //TODO:统一在用户信息这里处理初始数据
         ///// <summary>首次连接数据库时初始化数据，仅用于实体类重载，用户不应该调用该方法</summary>
         //[EditorBrowsable(EditorBrowsableState.Never)]
         //protected override void InitData()
@@ -211,9 +179,7 @@ namespace DotNet.CommonEntity
             if (Meta.Count >= 1000)
                 return Find(_.Id, id);
             else // 实体缓存
-                return Meta.Cache.Entities.Find(_.Id, id);
-            // 单对象缓存
-            //return Meta.SingleCache[id];
+                return Meta.Cache.Entities.Find(_.Id, id);          
         }
 
         /// <summary>根据员工编号查找</summary>
@@ -246,57 +212,16 @@ namespace DotNet.CommonEntity
         /// <param name="roleid">角色编号</param>
         /// <returns></returns>
         [DataObjectMethod(DataObjectMethodType.Select, false)]
-        public static EntityList<TEntity> FindAllByStaffIdAndRoleId(Int32 staffid, Int32 roleid)
+        public static TEntity FindByStaffIdAndRoleId(Int32 staffid, Int32 roleid)
         {
             if (Meta.Count >= 1000)
-                return FindAll(new String[] { _.StaffId, _.RoleId }, new Object[] { staffid, roleid });
+                return Find(new String[] { _.StaffId, _.RoleId }, new Object[] { staffid, roleid });
             else // 实体缓存
-                return Meta.Cache.Entities.FindAll(e => e.StaffId == staffid && e.RoleId == roleid);
-        }
-
-        /// <summary>根据角色编号查找</summary>
-        /// <param name="roleid">角色编号</param>
-        /// <returns></returns>
-        [DataObjectMethod(DataObjectMethodType.Select, false)]
-        public static EntityList<TEntity> FindAllByRoleId(Int32 roleid)
-        {
-            if (Meta.Count >= 1000)
-                return FindAll(_.RoleId, roleid);
-            else // 实体缓存
-                return Meta.Cache.Entities.FindAll(_.RoleId, roleid);
-        }
+                return Meta.Cache.Entities.Find(e => e.StaffId == staffid && e.RoleId == roleid);
+        }     
         #endregion
 
         #region 高级查询
-        // 以下为自定义高级查询的例子
-
-        ///// <summary>
-        ///// 查询满足条件的记录集，分页、排序
-        ///// </summary>
-        ///// <param name="key">关键字</param>
-        ///// <param name="orderClause">排序，不带Order By</param>
-        ///// <param name="startRowIndex">开始行，0表示第一行</param>
-        ///// <param name="maximumRows">最大返回行数，0表示所有行</param>
-        ///// <returns>实体集</returns>
-        //[DataObjectMethod(DataObjectMethodType.Select, true)]
-        //public static EntityList<TEntity> Search(String key, String orderClause, Int32 startRowIndex, Int32 maximumRows)
-        //{
-        //    return FindAll(SearchWhere(key), orderClause, null, startRowIndex, maximumRows);
-        //}
-
-        ///// <summary>
-        ///// 查询满足条件的记录总数，分页和排序无效，带参数是因为ObjectDataSource要求它跟Search统一
-        ///// </summary>
-        ///// <param name="key">关键字</param>
-        ///// <param name="orderClause">排序，不带Order By</param>
-        ///// <param name="startRowIndex">开始行，0表示第一行</param>
-        ///// <param name="maximumRows">最大返回行数，0表示所有行</param>
-        ///// <returns>记录数</returns>
-        //public static Int32 SearchCount(String key, String orderClause, Int32 startRowIndex, Int32 maximumRows)
-        //{
-        //    return FindCount(SearchWhere(key), null, null, 0, 0);
-        //}
-
         /// <summary>
         /// 构造搜索条件
         /// </summary>
@@ -309,13 +234,6 @@ namespace DotNet.CommonEntity
 
             // SearchWhereByKeys系列方法用于构建针对字符串字段的模糊搜索
             if (!String.IsNullOrEmpty(key)) SearchWhereByKeys(exp.Builder, key);
-
-            // 以下仅为演示，2、3行是同一个意思的不同写法，Field（继承自FieldItem）重载了==、!=、>、<、>=、<=等运算符（第4行）
-            //exp &= _.Name == "testName"
-            //    & !String.IsNullOrEmpty(key) & _.Name == key
-            //    .AndIf(!String.IsNullOrEmpty(key), _.Name == key)
-            //    | _.ID > 0;
-
             return exp;
         }
         #endregion
