e1e370a88ca320663524f39b7059dee0637be377
asxinyu@qq.com <asxinyu@qq.com@e81cc8ed-56e0-f867-87c4-5ecea47cd9ab>
2012-09-26 02:20:23 +0000

    基本后台完成

---
+++ trunk/Bioinfo/BioInfo.Web/WebSiteHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BioInfo.Web
+{
+    /// <summary>
+    /// 前台网页的参数查询
+    /// </summary>
+    public class WebSiteHelper
+    {
+    }
+}
